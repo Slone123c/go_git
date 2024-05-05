8ae8fa7930e8bc33f187f4e256c3a72c29385e5c
陈仕隆 <slone@Mac-mini.lan>
2024-05-05 15:44:54 +0800

    1st commit

---
+++ cmd/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"github.com/spf13/cobra"
+	"go_git/internal"
+)
+
+func main() {
+	var rootCmd = &cobra.Command{Use: "gogit"}
+	rootCmd.AddCommand(
+		internal.addCmd,
+	// addCmd,
+	// catFileCmd,
+	// checkIgnoreCmd,
+	// checkoutCmd,
+	// commitCmd,
+	// hashObjectCmd,
+	// initCmd,
+	// logCmd,
+	// lsFilesCmd,
+	// lsTreeCmd,
+	// revParseCmd,
+	// rmCmd,
+	// showRefCmd,
+	// statusCmd,
+	// tagCmd,
+	)
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ internal/addCmd.go
@@ -0,0 +1,17 @@
+package internal
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var addCmd = &cobra.Command{
+	Use:   "add [files]",
+	Short: "Add file contents to the index.",
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Adding files:", args)
+		// 调用实现添加文件到暂存区的函数
+	},
+}
